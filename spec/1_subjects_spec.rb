48d355742fda4f3778996321bfa905620615b9b1
Salomanuel <salomanuel.aux+code@gmail.com>
2018-07-02 15:46:21 +0200

    add Subjects

---
+++ spec/1_subjects_spec.rb
@@ -0,0 +1,17 @@
+class Person
+  attr_reader :first_name, :last_name
+
+  def initialize(first_name, last_name)
+    @first_name = first_name
+    @last_name = last_name
+  end
+end
+
+describe Person do
+  it 'creates a new person with a first and last name' do
+    person = Person.new 'John', 'Smith'
+
+    expect(person).to have_attributes(first_name: 'John')
+    expect(person).to have_attributes(last_name: 'Smith')
+  end
+end
